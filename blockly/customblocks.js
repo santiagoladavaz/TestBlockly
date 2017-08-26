c2b5740441507172c9d83da0e3cff15443a5ed34
Santiago Ladavaz <santiagoladavaz@gmail.com>
2017-08-26 19:50:13 -0300

    First Commit

---
+++ blockly/customblocks.js
@@ -0,0 +1,31 @@
+Blockly.Blocks['semaforo'] = {
+  init: function() {
+    this.appendStatementInput("Left")
+        .setCheck(null)
+        .appendField("Turn")
+        .appendField(new Blockly.FieldDropdown([["ROJO","R"], ["AMARILLO","A"], ["VERDE","V"]]), "Colores")
+        .appendField(new Blockly.FieldDropdown([["prender","prender"], ["apagar","apagar"]]), "Switch");
+    this.setInputsInline(false);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(210);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+
+//Generator Stub
+
+Blockly.JavaScript['semaforo'] = function(block) {
+  var colores = block.getFieldValue('Colores');
+  var dropdown_switch = block.getFieldValue('Switch');
+  var statements_left = Blockly.JavaScript.statementToCode(block, 'Left');
+  if(dropdown_switch == "prender"){
+    var code = "document.getElementbyId('circle').style.backGroundColor='red';";  
+  }
+  if(dropdown_switch == "apagar"){
+    var code = "document.getElementbyId('circle').style.backGroundColor='white';";  
+  }
+  return code;
+};
